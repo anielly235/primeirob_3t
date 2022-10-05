01395d97727213ca15c014e622df03fd6c8c8fa9
anielly235 <110236221+anielly235@users.noreply.github.com>
2022-10-05 12:46:42 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,4 +1,4 @@
-let tabuada = 6;
+let tabuada = 111;
 function escreva(){
     document.write("<h1> Tabuada do " + tabuada + "</h1>");
     document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
@@ -11,5 +11,24 @@ function escreva(){
     document.write(tabuada + " x 1 = " + (tabuada*8) + "<br>");
     document.write(tabuada + " x 1 = " + (tabuada*9) + "<br>");
     document.write(tabuada + " x 1 = " + (tabuada*10) + "<br>");
+}
+
+let lista = ["Jesus","Spessato","Duarte","Jorge","Neusa","Gilmar","Jossyara"];
+
+function mostraLista(){
+    document.write("Tamanho da Lista: " + lista.length + "<br>");
+    for(let i = 0; i < lista.length; i++){
+        document.write("Professor: " + lista[i] + "<br>")
+    } 
+}
+
+function multiplica(){
+    for(let i = 6; i <= 8; i++){
+        document,write("o valor do i = " + i + "<br>");
+        for(let j = 1; j <= 10; j++){
+            document.write(i + " x "+j+" = " + (i*j) + "<br>");
+        }
+        document.write("<br>");
+    }
 }
 
